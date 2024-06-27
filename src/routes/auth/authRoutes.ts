f1d4b509ecc855d7d15588587d866370cfdf1e72
Evan Kasky <ekasky25@gmail.com>
2024-06-27 09:33:38 -0400

    Adds input validation to login endpoint

--- src/routes/auth/authRoutes.ts
+++ src/routes/auth/authRoutes.ts
@@ -4,12 +4,13 @@ import validateRegisterRequest from '../../middleware/auth/validateRegisterReque
 import checkForRegisterConflicts from '../../middleware/auth/checkForRegisterConflicts';
 import verifyAccountController from '../../controllers/auth/verifyAccount';
 import loginController from '../../controllers/auth/loginController';
+import validateLoginRequest from '../../middleware/auth/validateLoginRequest';
 
 
 const authRouter = express.Router();
 
 authRouter.post('/register', validateRegisterRequest, checkForRegisterConflicts, registerController);
 authRouter.get('/verify-account/:token', verifyAccountController);
-authRouter.post('/login', loginController);
+authRouter.post('/login', validateLoginRequest, loginController);
 
 export default authRouter;
