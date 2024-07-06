72b52c3e530a1b820cb8ade72d41c1cf5cfc7ee6
Evan Kasky <ekasky25@gmail.com>
2024-07-06 02:08:42 -0400

    Completes register user endpoint
    	- If sanitize, validation, and conflict checks are all okay,
    	  hash the password and add the new user to the User's
    	  collection in the database

--- server/src/controllers/registerController.ts
+++ server/src/controllers/registerController.ts
@@ -1,7 +1,27 @@
+import bcrypt from 'bcrypt';
 import { Request, Response } from 'express';
+import User from '../models/User';
 
 export default async function registerController(req: Request, res: Response) {
   try {
+    // Exract the register fields from the request body
+    const { first_name, last_name, username, email, password } = req.body;
+
+    // Hash the user's password using bcrypt
+    const hash = await bcrypt.hash(password, 10);
+
+    // Create the new user
+    const user = new User({
+      first_name,
+      last_name,
+      username,
+      email,
+      password: hash,
+    });
+
+    // Save the new user to the db
+    await user.save();
+
     res.status(200).json({
       timestamp: Date.now(),
       message: 'User registered successfully',
